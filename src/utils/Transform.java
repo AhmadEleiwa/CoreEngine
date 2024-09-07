7f6d3d4fe46b65fe67dcb4fe1533e72d4c9429ea
ahmadeleiwa <ahmadilawa@gmai.com>
2024-09-07 14:26:42 +0300

    Add getter, setters, and operational methods for Transform attrubute, and setup obeserver

--- src/utils/Transform.java
+++ src/utils/Transform.java
@@ -1,19 +1,43 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.joml.Matrix4f;
 import org.joml.Vector3f;
 import org.joml.Quaternionf;
 
 public class Transform {
-    public Vector3f position;
-    public Quaternionf rotation;
-    public Vector3f scale;
+    protected Vector3f position;
+    protected Quaternionf rotation;
+    protected Vector3f scale;
+
+    private final List<Runnable> observers = new ArrayList<>();
 
     public Transform() {
-        position = new Vector3f(0.0f,
-                0.0f, 0.0f);
-        rotation = new Quaternionf(1.0f, 0.0f, 0.0f, 0.0f);
-        scale = new Vector3f(1.0f, 1.0f, 1.0f);
+        this.position = new Vector3f(0, 0, 0);
+        this.rotation = new Quaternionf();
+        this.scale = new Vector3f(1, 1, 1);
+    }
+
+    public Transform(Transform other) {
+        this.position = new Vector3f(other.position);
+        this.rotation = new Quaternionf(other.rotation);
+        this.scale = new Vector3f(other.scale);
+    }
+
+    public void addObserver(Runnable observer) {
+        observers.add(observer);
+    }
+
+    public void removeObserver(Runnable observer) {
+        observers.remove(observer);
+    }
+
+    private void notifyObservers() {
+        for (Runnable observer : observers) {
+            observer.run();
+        }
     }
 
     public Matrix4f getTransformationMatrix() {
@@ -22,6 +46,157 @@ public class Transform {
         matrix.rotate(rotation);
         matrix.scale(scale);
         return matrix;
+    }
+
+    public void setTransform(Transform transform) {
+        this.position = new Vector3f(transform.position);
+        this.rotation = new Quaternionf(transform.rotation);
+        this.scale = new Vector3f(transform.scale);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Transform other = (Transform) obj;
+        return position.equals(other.position) && scale.equals(other.scale) && rotation.equals(other.rotation);
+    }
+
+    public Vector3f getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector3f position) {
+        this.position = position;
+        notifyObservers();
+    }
+
+    public void setPositionX(float x) {
+        this.position.x = x;
+        notifyObservers();
+    }
+
+    public void setPositionY(float y) {
+        this.position.y = y;
+        notifyObservers();
+    }
+
+    public void setPositionZ(float z) {
+        this.position.z = z;
+        notifyObservers();
+    }
+
+    public void move(Vector3f vector3f) {
+        this.position.add(vector3f);
+        notifyObservers();
+    }
+
+    public void moveX(float x) {
+        this.position.x += x;
+        notifyObservers();
+    }
+
+    public void moveY(float y) {
+        this.position.y += y;
+        notifyObservers();
+    }
+
+    public void moveZ(float z) {
+        this.position.z += z;
+        notifyObservers();
+    }
+
+    // Rotation Handling
+
+    public Quaternionf getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(Quaternionf rotation) {
+        this.rotation = rotation;
+        notifyObservers();
+    }
+
+    public void setRotationX(float angle) {
+        this.rotation.rotateX(angle);
+        notifyObservers();
+    }
+
+    public void setRotationY(float angle) {
+        this.rotation.rotateY(angle);
+        notifyObservers();
+    }
+
+    public void setRotationZ(float angle) {
+        this.rotation.rotateZ(angle);
+        notifyObservers();
+    }
+
+    public void rotate(Vector3f rotation, float angle) {
+        this.rotation.rotateAxis(angle, rotation);
+        notifyObservers();
+    }
+
+    public void rotateX(float angleX) {
+        this.rotation.rotateX(angleX);
+        notifyObservers();
+    }
+
+    public void rotateY(float angleY) {
+        this.rotation.rotateY(angleY);
+        notifyObservers();
+    }
+
+    public void rotateZ(float angleZ) {
+        this.rotation.rotateZ(angleZ);
+        notifyObservers();
+    }
+    // Scale Handling
+
+    public Vector3f getScale() {
+        return scale;
+    }
+
+    public void setScale(Vector3f scale) {
+        this.scale = scale;
+        notifyObservers();
+    }
+
+    public void setScaleX(float x) {
+        this.scale.x = x;
+        notifyObservers();
+    }
+
+    public void setScaleY(float y) {
+        this.scale.y = y;
+        notifyObservers();
+    }
+
+    public void setScaleZ(float z) {
+        this.scale.z = z;
+        notifyObservers();
+    }
+
+    public void addScale(Vector3f vector3f) {
+        this.scale.add(vector3f);
+        notifyObservers();
+    }
+
+    public void addScaleX(float x) {
+        this.scale.x += x;
+        notifyObservers();
+    }
+
+    public void addScaleY(float y) {
+        this.scale.y += y;
+        notifyObservers();
+    }
+
+    public void addScaleZ(float z) {
+        this.scale.z += z;
+        notifyObservers();
+    }
+
+    public List<Runnable> getObservers() {
+        return observers;
     }
 
 }
