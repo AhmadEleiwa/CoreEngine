b0b2546c72f083b248a901cb8f9da75a4fcc2989
ahmadeleiwa <ahmadilawa@gmai.com>
2024-09-07 14:29:08 +0300

    Remove Shader attrbute and  add upateView method

--- src/utils/Camera.java
+++ src/utils/Camera.java
@@ -4,31 +4,67 @@ import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
 import engine.GameEngine;
-import engine.ProgramManager;
 import graphics.Renderer;
 import scenes.Node;
 import scenes.ShaderProgram;
 
-public class Camera extends Node{
-    protected ShaderProgram shader;
+public class Camera extends Node {
     protected Transformation transformation;
+    // Target point to look at
+    protected Vector3f target = new Vector3f(0, 0, 0);
+
+    protected Matrix4f viewMatrix4f;
+    protected Matrix4f projectionMatrix4f;
+    protected GameEngine gameEngine;
 
     public Camera(String name) {
         super(name);
-        //TODO Auto-generated constructor stub
-        GameEngine gameEngine = GameEngine.getInstance();
+        // TODO Auto-generated constructor stub
 
-        this.shader = gameEngine.createShaderProgram("default", "assets/textureShader/vertix.glsl","assets/textureShader/fragment.glsl");
+        this.gameEngine = GameEngine.getInstance();
         transformation = new Transformation();
+        projectionMatrix4f = transformation.getProjectionMatrix(70, 800, 600, 0.1f, 100);
+        localTransform.setPosition(new Vector3f(0, 0, 10));
+        viewMatrix4f = new Matrix4f().identity().lookAt(localTransform.position, target, up);
     }
+
     @Override
     public void render(Renderer renderer) {
-        shader.start();
+
+        target = new Vector3f(globalTransform.position);
+        target.z -= 1;
+        viewMatrix4f = new Matrix4f().identity().lookAt(globalTransform.position, target, up);
         updateUniforms();
-        shader.setUniformMatrix4f("view", new Matrix4f().identity().translate(new Vector3f(0,0,-5)));
-        shader.setUniformMatrix4f("projection", transformation.getProjectionMatrix(70, 800, 600, 0.1f, 100));
-        shader.stop();
+
         super.render(renderer);
     }
-    
+
+    public void updateViewByShader(ShaderProgram shader) {
+        shader.setUniformMatrix4f("view", viewMatrix4f);
+        shader.setUniformMatrix4f("projection", transformation.getProjectionMatrix(70, 800, 600, 0.1f, 100));
+    }
+
+    @Override
+    protected void updateUniforms() {
+        super.updateUniforms();
+    }
+
+    public Vector3f getTarget() {
+        return target;
+    }
+
+    public void setTarget(Vector3f target) {
+        this.target = target;
+    }
+
+    // Up direction (usually along the Y axis)
+    protected Vector3f up = new Vector3f(0, 1, 0);
+
+    public Vector3f getUp() {
+        return up;
+    }
+
+    public void setUp(Vector3f up) {
+        this.up = up;
+    }
 }
